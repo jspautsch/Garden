0dc39b2bd1eeabd49b2ef94def70887fdce8ae3f
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-07-29 15:56:22 -0400

    Comment

--- applications/vanilla/controllers/class.categoriescontroller.php
+++ applications/vanilla/controllers/class.categoriescontroller.php
@@ -7,12 +7,7 @@ Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRAN
 You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
 Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
 */
-/**
- * Categories Controller
- *
- * @package Vanilla
- */
- 
+
 /**
  * Handles displaying categories.
  *
